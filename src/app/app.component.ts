9d8ee28b51f5948841fa8ba4c3dba4abca9f3cfd
Ricardo Pérez <rpe19971210@gmail.com>
2022-09-06 17:37:11 -0300

    mostrando datos del login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
 export class AppComponent {
-  title = 'Blog de Angular';
+  public title = 'Blog de Angular';
+  public identity: any;
+  public token: any;
+
+  constructor(
+    public _userService: UserService
+  ){
+    this.identity = this._userService.getIdentity();
+  }
 }
