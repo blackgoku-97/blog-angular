a3536ad9e4f72a7362c92759f9fcc6d2c6e653b6
Ricardo Pérez <rpe19971210@gmail.com>
2022-09-06 00:39:06 -0300

    desarrollo login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,19 +11,52 @@ import { UserService } from '../../services/user.service';
 export class LoginComponent implements OnInit {
   public page_title: string;
   public user: User;
+  public status: string;
+  public token: any;
+  public identity: any;
 
   constructor(
     private _userService: UserService
   ) {
     this.page_title = 'Identificate';
     this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
+    this.status = '';
   }
 
   ngOnInit(): void {
   }
 
   onSubmit(form: any){
-    console.log(this.user);
+    this._userService.signup(this.user).subscribe(
+      response => {
+        // TOKEN
+        if(response.status != 'error'){
+          this.status = 'success';
+          this.token = response;
+
+          // OBJETO USUARIO IDENTIFICADO
+          this._userService.signup(this.user, this.token).subscribe(
+            response => {
+              this.identity = response;
+
+              console.log(this.token);
+              console.log(this.identity);
+            },
+            error => {
+              this.status = 'error';
+              console.log(<any>error);
+            }
+          );
+
+        }else{
+          this.status = 'error';
+        }
+      },
+      error => {
+        this.status = 'error';
+        console.log(<any>error);
+      }
+    );
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -28,4 +28,17 @@ export class UserService {
 
     return this._http.post(this.url+'register', params, {headers: headers});
   }
+
+  signup(user: any, getToken = null): Observable<any>{
+    if(getToken != null){
+      user.getToken = 'true';
+    }
+
+    let json = JSON.stringify(user);
+    let params = 'json='+json;
+
+    let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+    return this._http.post(this.url+'login', params, {headers: headers});
+  }
 }
